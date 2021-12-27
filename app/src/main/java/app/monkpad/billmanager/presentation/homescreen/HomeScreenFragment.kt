af1b8a1d35ff667c8785ebcb98c5442659b8e914
wanjala <gwiliez@gmail.com>
2021-12-27 22:50:20 +0300

    design the home screen - add recyclerview with tabs -design the balance card - hook bottom nav to flow

--- app/src/main/java/app/monkpad/billmanager/presentation/homescreen/HomeScreenFragment.kt
+++ app/src/main/java/app/monkpad/billmanager/presentation/homescreen/HomeScreenFragment.kt
@@ -5,21 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import app.monkpad.billmanager.R
 import app.monkpad.billmanager.databinding.FragmentHomeScreenBinding
-
 
 class HomeScreenFragment : Fragment() {
 
-
+    private lateinit var mainCollectionsAdapter: MainRecyclerAdapter
+    private lateinit var binding: FragmentHomeScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        binding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mainCollectionsAdapter = MainRecyclerAdapter()
+        binding.mainScreenRecycler.adapter = mainCollectionsAdapter
     }
 
 
